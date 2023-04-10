4ad5f374ccb893ec90ce5ed28199b6f5bd61043e
miha <miha2005r@mail.ru>
2023-04-10 18:12:20 +0300

    rfgtrf

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.uix.image import Image
 from kivy.core.text import LabelBase
 from kivy.uix.screenmanager import ScreenManager
 from kivy_garden.mapview import MapView
@@ -9,6 +10,8 @@ Window.size = (310,580)
 class MyApp(MDApp):
     def build(self):
         self.screen = Builder.load_file("main.kv")
+        self.screen = Builder.load_file("profileuser.kv")
+        self.screen = Builder.load_file("reviews.kv")
         return self.screen
 
 
