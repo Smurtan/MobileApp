172f7cf2b5b8e42de871c480120815d53d9f2285
Artem Osokin <astapova.i.00@gmail.com>
2023-04-10 19:30:00 +0300

    add_all

--- main2.py
+++ main2.py
@@ -1,12 +1,17 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy_garden.mapview import MapView
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 
 
 class MyApp(MDApp):
     def build(self):
-        self.screen = Builder.load_file("main.kv")
-        return self.screen
+        self.screen_menu = ScreenManager()
+        self.screen_menu.add_widget(Builder.load_file("main.kv"))
+        self.screen_menu.add_widget(Builder.load_file("Reviews.kv"))
+        self.screen_menu.add_widget(Builder.load_file("ProfileScreen.kv"))
+        return self.screen_menu
 
 
 MyApp().run()
