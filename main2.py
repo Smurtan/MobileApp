f6c7e4e2d6b54bbcb477cdb728c0e65294385a7a
miha <miha2005r@mail.ru>
2023-04-11 19:15:00 +0300

    сделал настрйоки, окно смены пороля !начал делать данные!

--- main2.py
+++ main2.py
@@ -8,11 +8,12 @@ class MyApp(MDApp):
     def build(self):
         self.screen_menu = ScreenManager()
         self.screen_menu.add_widget(Builder.load_file("main.kv"),)
-        self.screen_menu.add_widget(Builder.load_file("Reviews.kv"))
+        #self.screen_menu.add_widget(Builder.load_file("Reviews.kv"))
         self.screen_menu.add_widget(Builder.load_file("ProfileScreen.kv"))
         self.screen_menu.add_widget(Builder.load_file("add_trip.kv"))
         self.screen_menu.add_widget(Builder.load_file("setings.kv"))
         self.screen_menu.add_widget(Builder.load_file("password.kv"))
+        self.screen_menu.add_widget(Builder.load_file("dan.kv"))
         return self.screen_menu
 
 
